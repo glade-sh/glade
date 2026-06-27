c76d85161b864dd4d32c72ab34d63d547798881e
mattsimonis <720686+mattsimonis@users.noreply.github.com>
2026-06-27 16:01:03 -0700

    chore: prove terminal ui dependency fit

---
+++ internal/tui/library_probe_test.go
@@ -0,0 +1,17 @@
+package tui
+
+import (
+	"testing"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type probeModel struct{}
+
+func (probeModel) Init() tea.Cmd { return nil }
+func (probeModel) Update(tea.Msg) (tea.Model, tea.Cmd) { return probeModel{}, nil }
+func (probeModel) View() string { return "glade tui" }
+
+func TestBubbleTeaProbeCompiles(t *testing.T) {
+	var _ tea.Model = probeModel{}
+}
